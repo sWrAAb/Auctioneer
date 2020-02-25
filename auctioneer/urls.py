3ec0ddc8771a2aa4725ba44ff732e5e02b4af733
sWrAAb <swraab@gmail.com>
2020-02-25 17:07:17 +0000

    added login form

--- auctioneer/urls.py
+++ auctioneer/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from accounts.views import index
+from accounts.views import index, logout, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name="index"),
+    path('accounts/logout/', logout, name="logout"),
+    path('accounts/login/', login, name="login"),
 ]
