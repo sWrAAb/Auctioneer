37eb38ba45070625c269d61fbb88774921577a5e
sWrAAb <swraab@gmail.com>
2020-02-26 17:02:29 +0000

    added cart app

--- auctioneer/urls.py
+++ auctioneer/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('accounts/', include('accounts.urls')),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('cart/', include('cart.urls'))
+
 ]
