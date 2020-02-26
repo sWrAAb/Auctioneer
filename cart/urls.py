2f1c12c1dc258d91e79c428e9f64cf532f7510ee
sWrAAb <swraab@gmail.com>
2020-02-26 17:46:57 +0000

    finished cart app

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,6 @@ from .views import view_cart, add_to_cart, adjust_cart
 
 urlpatterns = [
     path('', view_cart, name='view_cart'),
-    path('add/(<id>)', add_to_cart, name='add_to_cart'),
+    path('add/<id>', add_to_cart, name='add_to_cart'),
     path('adjust/<id>', adjust_cart, name='adjust_cart'),
 ]
