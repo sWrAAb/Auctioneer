a7fd8af1fae5458d507f252b0f91fff31e793a80
sWrAAb <swraab@gmail.com>
2020-02-29 22:58:52 +0000

    autoprefixed css and added few lines in readme.MD.

--- auctioneer/settings.py
+++ auctioneer/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = [ 'the-auctioneer.herokuapp.com']
+ALLOWED_HOSTS = [ 'the-auctioneer.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
